2941ec7b151deb37c620214e69706274fcc16329
Nicofatch <judge_nic@yahoo.fr>
2013-08-22 05:28:36 +0200

    Initial commit

---
+++ web.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var spot = require('./api/spot');
+var app = express();
+//app.use(express.logger());
+
+app.use(function(req,res,next) {
+    console.log('%s %s', req.method, req.url);
+    next();
+});
+
+app.use(express.methodOverride());
+
+app.use(express.bodyParser());
+
+// ## CORS middleware
+// see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
+
+//API
+app.get('/api/spots',spot.findAll);
+app.get('/api/spots/:id',spot.findById);
+app.post('/api/spots',spot.add);
+app.put('/api/spots/:id',spot.update);
+app.delete('/api/spots/:id',spot.delete);
+
+var port = process.env.PORT || 5001;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
