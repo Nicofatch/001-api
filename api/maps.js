c4d02de76741a482bd3e922a6ac97a60e2fb7cfc
Nicofatch <judge_nic@yahoo.fr>
2013-10-08 11:55:39 +0200

    test

--- api/maps.js
+++ api/maps.js
@@ -1,23 +1,4 @@
-var mongo = require('mongodb');
-
-var Server = mongo.Server,
-Db = mongo.Db,
-BSON = mongo.BSONPure;
-
-var server = new Server('localhost', 27017, {auto_reconnect: true});
-db = new Db('spotdb', server);
-
-db.open(function(err, db) {
-    if(!err) {
-        console.log("Connected to 'spotdb' database");
-        db.collection('maps', {strict:true}, function(err, collection) {
-            if (err) {
-                console.log("The 'maps' collection doesn't exist. Creating it with sample data...");
-                populateDB();
-            }
-        });
-    }
-});
+var tags = require('./tags.js');
 
 exports.findById = function(req, res) {
     var id = req.params.id;
@@ -50,6 +31,10 @@ exports.add = function(req, res) {
             } else {
                 console.log('Success: ' + JSON.stringify(result[0]));
                 res.send(result[0]);
+		//update tags
+		for (var i=0,l=map.tags.length;i<l;i++) {
+		    tags.addIfNew(map.tags[i]);
+		}
             }
         });
     });
@@ -70,6 +55,10 @@ exports.update = function(req, res) {
                 console.log('' + result + ' document(s) updated');
                 map._id = id;
 		res.send(map);
+		//update tags
+		for (var i=0,l=map.tags.length;i<l;i++) {
+		    tags.addIfNew(map.tags[i]);
+		}
             }
         });
     });
@@ -89,91 +78,4 @@ exports.delete = function(req, res) {
         });
     });
 };
-
-/*--------------------------------------------------------------------------------------------------------------------*/
-// Populate database with sample data -- Only used once: the first time the application is started.
-// You'd typically not find this code in a real-life app, since the database would already exist.
-var populateDB = function() {
-    
-    var spots = [
-	{
-	    "title": "paintball75",
-	    "description": "Un super terrain de paintball",
-	    "sports": "paintball",
-	    "longitude": 2.340841,
-	    "latitude": 48.8650429,
-	    "_id": "1"
-	},
-	{
-	    "title": "cinema de suresnes",
-	    "description": "Le meilleur cinema du grand Ouest",
-	    "sports": "cinema",
-	    "longitude": 2.242201,
-	    "latitude": 48.8649466,
-	    "_id": "2"
-	},
-	{
-	    "title": "bowling du chaton",
-	    "description": "Le meilleur bowling pour chats",
-	    "sports": "bowling",
-	    "longitude": 2.302201,
-	    "latitude": 48.9649466,
-	    "_id": "3"
-	}
-    ];
-
-    var maps = [
-	{
-	    "title": "Les bars à chat de Paris",
-	    "description": "Carte des meilleurs bars parisiens dans lesquels votre minou sera chouchouté",
-	    "tags": ['bar','chat','paris'],
-	    "contributors_count": "12",
-	    "markers_count": "63",
-	    "spots": spots,
-	    "creationDate": "",
-	    "modificationDate" : "",
-	    "private": true 
-	},
-	{
-	    "title": "Les hotels à chat",
-	    "description": "Carte des meilleurs hotels pour chat",
-	    "tags": ['chat','hotel'],
-	    "contributors_count": "4",
-	    "markers_count": "15",
-	    "spots": [],
-	    "creationDate": "",
-	    "modificationDate" : "",
-	    "private": false
-	},
-	{
-	    "title": "Les plus beau chats",
-	    "description": "Spottez votre chat. Trop mignon !!",
-	    "tags": ['beauté','chat','mignon', 'grraou'],
-	    "contributors_count": "1274",
-	    "markers_count": "1274",
-	    "spots": spots,
-	    "creationDate": "",
-	    "modificationDate" : "",
-	    "private": true
-	},
-	{
-	    "title": "Les plus beau chiens",
-	    "description": "Spottez votre chien. Trop mignon !!",
-	    "tags": ['beauté','chien','mignon'],
-	    "contributors_count": "643",
-	    "markers_count": "643",
-	    "spots": spots,
-	    "creationDate": "",
-	    "modificationDate" : "",
-	    "private": false
-	}];
- 
-    db.collection('maps', function(err, collection) {
-	console.log('good job');
-	collection.insert(maps, {safe:true}, function(err, result) {});
-    });
-
-};
-
-
 
--- web.js
+++ web.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var maps = require('./api/maps');
+var tags = require('./api/tags');
 var app = express();
 //app.use(express.logger());
+
+var connection = require('./api/connection.js');
+
+// global variables
+BSON = connection.BSON;
+db = connection.db;
 
 app.use(function(req,res,next) {
     console.log('%s %s', req.method, req.url);
@@ -41,6 +48,13 @@ app.get('/api/maps/:id',maps.findById);
 app.post('/api/maps',maps.add);
 app.put('/api/maps/:id',maps.update);
 app.delete('/api/maps/:id',maps.delete);
+
+app.get('/api/tags',tags.findAll);
+app.get('/api/tags/search',tags.findByValue);
+app.get('/api/tags/:id',tags.findById);
+app.post('/api/tags',tags.add);
+app.put('/api/tags/:id',tags.update);
+app.delete('/api/tags/:id',tags.delete);
 
 
 var port = process.env.PORT || 5001;
